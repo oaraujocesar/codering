d9f245575ffd1cc39d742b5f29d15a4c2be009e7
César O. Araújo <oaraujocesar@gmail.com>
2021-04-02 10:53:34 -0300

    feat: :construction: changing icons and manifest

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,13 +6,15 @@ import GlobalStyles from 'styles/global'
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <>
     <Head>
-      <title>React Avançado - Boilerplate</title>
+      <title>
+        Codering - Sua melhor fonte de informação sobre desenvolvimento
+      </title>
       <link rel="shortcut icon" href="/img/icon-512.png" />
       <link rel="apple-touch-icon" href="/img/icon-512.png" />
       <link rel="manifest" href="/manifest.json" />
       <meta
         name="description"
-        content="A simple starter project to work with TypeScript, React, NextJS and Styled Components"
+        content="Um blog para espalhar conhecimento sobre tecnologia de maneira descomplicada e interativa."
       />
     </Head>
     <GlobalStyles />
