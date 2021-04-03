2d3bb8ba81ec473a42b39ddcd1cba4ff8c9388eb
César O. Araújo <oaraujocesar@gmail.com>
2021-04-03 14:10:46 -0300

    feat: :memo: adding the switch component story

--- src/components/Switch/stories.tsx
+++ src/components/Switch/stories.tsx
@@ -1,10 +1,23 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
+import { theme } from '../../styles/theme'
+import { ThemeProvider } from 'styled-components'
 
 import Switch from '.'
 
 export default {
-  title: 'Switch',
-  component: Switch
+  title: 'Theme Switch',
+  component: Switch,
+  argTypes: {
+    currentTheme: { control: { type: 'radio', options: ['light', 'dark'] } }
+  }
 } as Meta
 
-export const Basic: Story = (args) => <Switch {...args} />
+export const Basic: Story = (args) => {
+  const currentTheme = args.currentTheme === 'dark' ? theme.dark : theme.light
+
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <Switch {...args} />
+    </ThemeProvider>
+  )
+}
